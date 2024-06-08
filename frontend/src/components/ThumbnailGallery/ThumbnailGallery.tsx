0cb9387b3f57ebc381bf9b0bbc649b7e3cb6dfc0
Jesus Exposito <susucks@gmail.com>
2024-06-08 14:08:23 +0200

    refactor ThumbnailGallery to open thumbnails in modals to see them bigger

--- frontend/src/components/ThumbnailGallery/ThumbnailGallery.tsx
+++ frontend/src/components/ThumbnailGallery/ThumbnailGallery.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styles from './ThumbnailGallery.module.css'
 
 interface ThumbnailGalleryProps {
@@ -8,16 +9,44 @@ interface ThumbnailGalleryProps {
 //TODO :: make thumbnail open in a model to see the image in full-screen
 
 const ThumbnailGallery: React.FC<ThumbnailGalleryProps> = ({ thumbnails, removeImage }) => {
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
+  const openModal = (image: string) => {
+    setSelectedImage(image);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setSelectedImage(null);
+    setIsModalOpen(false);
+  };
+
+
   return (
-    <div className={styles.thumbnailGallery}>
-      {thumbnails.map((image, index) => (
-        <div key={index} className={styles.thumbnailContainer}>
-          <img src={image} alt={`Thumbnail ${index}`} className={styles.thumbnail} />
-          <button className={styles.closeButton} onClick={() => removeImage(index)}>
-            &times;
-          </button>
-        </div>
-      ))}
+    <div>
+      <div className={styles.thumbnailGallery}>
+        {thumbnails.map((image, index) => (
+          <div key={index} className={styles.thumbnailContainer}>
+            <img src={image} alt={`Thumbnail ${index}`} className={styles.thumbnail} onClick={() => openModal(image)} />
+            <button className={styles.closeButton} onClick={() => removeImage(index)}>
+              &times;
+            </button>
+          </div>
+        ))}
+      </div>
+
+      {isModalOpen && selectedImage && (
+          <div className={styles.modalOverlay} onClick={closeModal}>
+            <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
+              <img src={selectedImage} alt="Selected" className={styles.modalImage} />
+              <button onClick={closeModal} className={styles.closeButton}>
+                &times;
+              </button>
+            </div>
+          </div>
+        )}
     </div>
   );
 };
